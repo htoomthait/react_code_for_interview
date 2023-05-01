fcad79597653dc8cae9da739735b2ff7714c92bb
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-05-02 02:11:27 +0630

    can list down, update and delete from ui to json server

---
+++ react-query-tailwind-vite-ref-dave-gray/src/features/todos/TodoList.jsx
@@ -0,0 +1,112 @@
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getTodos, addTodo, updateTodo, deleteTodo } from "../../apis/todosApi";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+const TodoList = () => {
+    const [newTodo, setNewTodo] = useState("");
+    const queryClient = useQueryClient();
+
+    const {
+        isLoading,
+        isError,
+        error,
+        data: todos,
+    } = useQuery("todos", getTodos, {
+        select: (data) => data.sort((a, b) => b.id - a.id),
+    });
+
+    const addTodoMutation = useMutation(addTodo, {
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries("todos");
+        },
+    });
+
+    const updateTodoMutation = useMutation(updateTodo, {
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries("todos");
+        },
+    });
+
+    const deleteTodoMutation = useMutation(deleteTodo, {
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries("todos");
+        },
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodoMutation.mutate({ userId: 1, title: newTodo, completed: false });
+        setNewTodo("");
+    };
+
+    const newItemSection = () => (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="new-todo"> Enter a new todo item </label>
+            <div className="new-todo">
+                <input
+                    type="text"
+                    id="new-todo"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Enter new todo"
+                />
+            </div>
+            <buttton className="submit">
+                <FontAwesomeIcon icon={faTrash} />
+            </buttton>
+        </form>
+    );
+
+    let content;
+
+    if (isLoading) {
+        content = <p>Loading...</p>;
+    } else if (isError) {
+        content = <p>{error.message}</p>;
+    } else {
+        content = todos.map((todo) => {
+            return (
+                <article key={todo.id}>
+                    <div className="todo">
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() =>
+                                updateTodoMutation.mutate({
+                                    ...todo,
+                                    completed: !todo.completed,
+                                })
+                            }
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button
+                        className="trash"
+                        onClick={() =>
+                            deleteTodoMutation.mutate({ id: todo.id })
+                        }
+                    >
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            );
+        });
+    }
+
+    return (
+        <div>
+            <h1 className="text-red-400">Todo List</h1>
+            {newItemSection}
+            {content}
+        </div>
+    );
+};
+
+export default TodoList;
