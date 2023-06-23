446584875c3e7da293f03fdda541997c0bb2b780
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-06-23 16:25:27 +0630

    recoil root optimized at app

--- react-vite-recoil/src/App.jsx
+++ react-vite-recoil/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import TodoList from "./components/TodoList";
-import { RecoilRoot } from "recoil";
 
 function App() {
     const h1Color = "#61dafb";
@@ -8,9 +7,7 @@ function App() {
         <div className="app">
             <h1 style={{ color: `${h1Color}` }}>Recoil Example</h1>
             <h2>Learn Recoil Example with Simple todo list app</h2>
-            <RecoilRoot>
-                <TodoList />
-            </RecoilRoot>
+            <TodoList />
         </div>
     );
 }
