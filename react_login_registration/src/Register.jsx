d2c04c02aef165323059cbd01c24d5fa97590857
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-08-31 16:46:59 +0630

    username input wrote

--- react_login_registration/src/Register.jsx
+++ react_login_registration/src/Register.jsx
@@ -99,7 +99,53 @@ const Register = () => {
                     </p>
                 </section>
             ) : (
-                <section></section>
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>
+                        {errMsg}
+                    </p>
+
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">
+                            Username:
+                            <FontAwsomeIcon
+                                icon={faCheck}
+                                className={validName ? "valid" : "hide"}
+                            />
+                            <FontAwsomeIcon
+                                icon={faTimes}
+                                className={
+                                    validName || !user ? "hide" : "invalid"
+                                }
+                            />
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                            aria-invalid={validName ? "false" : "true"}
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p
+                            id="uidnote"
+                            className={
+                                userFocus && user && !validName
+                                    ? "instructions"
+                                    : "offscreen"
+                            }
+                        >
+                            <FontAwsomeIcon icon={faInfoCircle} />4 to 24
+                            characters, letters and numbers only <br />
+                            Must begin with a letter <br />
+                            Letters, numbers, underscores, hyphens allowed
+                        </p>
+                    </form>
+                </section>
             )}
         </>
     );
