264f83df4c16f020780114cca6a58fa9d7db1f31
Htoo Maung Thait <hmthait@blueplanetmm.com>
2023-11-11 09:46:34 +0630

    blog page display done

---
+++ react_vite_blog/src/dummy_data/featuredPost.js
@@ -0,0 +1,29 @@
+const mainFeaturedPost = {
+    title: 'Title of a longer featured blog post',
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: 'https://source.unsplash.com/random?wallpapers',
+    imageText: 'main image description',
+    linkText: 'Continue reading…',
+  };
+
+  const featuredPosts = [
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageLabel: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      image: 'https://source.unsplash.com/random?wallpapers',
+      imageLabel: 'Image Text',
+    },
+  ];
+
+export {mainFeaturedPost, featuredPosts}
