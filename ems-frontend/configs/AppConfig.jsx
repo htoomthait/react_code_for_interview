0d0f029241b1e618ad79fa2574ffb73937be92d2
Htoo Maung Thait <46110517+htoomgt@users.noreply.github.com>
2024-04-05 11:06:31 +0700

    Create AppConfig.jsx
    App Config created with backend base url.

---
+++ ems-frontend/configs/AppConfig.jsx
@@ -0,0 +1,11 @@
+const AppConfig = () => {
+  
+  const backendBaseURL = import.meta.env.VITE_BACKEND_BASE_URL";
+
+  return {
+    backendBaseURL: backendBasedURL
+  };
+}
+
+export default AppConfig;
+
