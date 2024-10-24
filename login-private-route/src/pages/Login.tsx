2c11e87f07308c9264425f3190cd6c13df605405
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-10-25 02:22:50 +0630

    form validation at react js part done

--- login-private-route/src/pages/Login.tsx
+++ login-private-route/src/pages/Login.tsx
@@ -1,72 +1,152 @@
 import React, {useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import _ from "lodash";
 
 interface LoginProps {
     setIsAuthenticated : (status : boolean) => void;
 }
 
+interface UserLogin{
+  username : string;
+  password : string;
+}
+const formData : UserLogin = { username: '', password: '' };
+const errorInit : UserLogin = { username: '', password: '' };
+
+
 
 const Login : React.FC<LoginProps> = ({setIsAuthenticated}) => {
-    const [username, setUsername] = useState('');
-    const [usernameError, setUsernameError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [password, setPassword] = useState('');
+    
+
+    const [values, setValues] = useState(formData);
+    const [errors , setErrors] = useState(errorInit);
+    const [isValidForm, setIsValidForm] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isInitialForm, setIsInitialForm] = useState(true);
+
+
     const navigate = useNavigate();
 
     
-    const handleSubmit = async (e : any) => {
+
+    const validateForm = () => {
+      let validationErrors = errorInit;
+
+      if(values.username == ""){        
+        validationErrors.username = "username is missing";
+        setIsValidForm(false);
+      }else{
+        validationErrors.username = "";        
+      }
+
+      if(values.password == ""){        
+        validationErrors.password = "password is missing!";
+        setIsValidForm(false);
+
+      }else{        
+        validationErrors.password = "";
+      }
+      setErrors((prevValues) => ({...prevValues, ...validationErrors}));
+      
+      let validFormStatus = false;
+      for (const [key, value] of Object.entries(validationErrors)) {
+        if(value != ""){
+          validFormStatus = false;
+          break;
+        }
+        else{
+          validFormStatus = true;
+        }
+      }
+
+      setIsValidForm(validFormStatus)
+
+      
+      return validFormStatus;
+      
+    }
+    const debounceValidateFields = _.debounce(() => { validateForm() }, 500)
+
+    const handleOnChange = (e : React.ChangeEvent<HTMLInputElement> ) => {
+      e.preventDefault();
+      
+      
+      setValues((prevValues) => ({ ...prevValues, [e.target.name]: e.target.value }));
+
+      
+      if(!isInitialForm){
+        debounceValidateFields();  
+      }
+      
+      
+
+    }
+    
+
+    
+    const handleSubmit = async (e : React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
 
-        // Dummy API call for login. Replace with your API endpoint.
+                
+        setIsSubmitting(true);
+        setIsInitialForm(false);
+
+
+        if(validateForm() ){
+          // Dummy API call for login. Replace with your API endpoint.
         const response = await fetch('http://localhost:8000/api/authenticate', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
+          method: 'POST',
+          headers: {
+          'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...values }),
+      });
 
-        if (response.status == 200) {
-            const data = await response.json();
-            // Assuming the response contains an access_token
-            localStorage.setItem('access_token', data.access_token);
-            setIsAuthenticated(true);
-            navigate('/admin');
-          } 
-          else if(response.status == 422){
-            const msgBody = await response.json();
+      setIsSubmitting(false);
 
-            msgBody.errorMsg.map((errMsg : any) => {
-                console.log(errMsg);
+      if (response.status == 200) {
+          const data = await response.json();
+          // Assuming the response contains an access_token
+          localStorage.setItem('access_token', data.access_token);
+          setIsAuthenticated(true);
+          navigate('/admin');
+        } 
+        else if(response.status == 422){
+          const msgBody = await response.json();
 
-                if(errMsg.username){
-                    console.log("has username error")
-                    setUsernameError(errMsg.username);
-                }
+          msgBody.errorMsg.map((errMsg : any) => {
+              console.log(errMsg);
 
-                if(errMsg.password){
-                    setPasswordError(errMsg.password);
-                }
-            })
+              if(errMsg.username){
+                  setErrors((prevValues) => ({...prevValues, username : errMsg.username}));
+              }
 
-           
-            
+              if(errMsg.password){
+                  setErrors((prevValues) => ({...prevValues, password : errMsg.password}));
+              }
+          })
+
+         
+          
 
 
-          }
-          else if(response.status == 403){
-            const msgBody = await response.json();
-            // console.log(msgBody.message);
+        }
+        else if(response.status == 403){
+          const msgBody = await response.json();
+          // console.log(msgBody.message);
 
-            alert(msgBody.message);
-          }
-          else {
-            alert('Login failed, please try again.');
-          }
-  
+          alert(msgBody.message);
+        }
+        else {
+          alert('Login failed, please try again.');
+        }
+        }
+
+        
       
         
     }
+
 
     
 
@@ -80,24 +160,24 @@ const Login : React.FC<LoginProps> = ({setIsAuthenticated}) => {
             <h2 className='font-bold text-xl'> System Login</h2>
             <form onSubmit={handleSubmit}>
             <div className="flex flex-col">
-                <label htmlFor="txtUsername" >Username</label>
-                <input type="text" value={username} name="txtUsername" onChange={(e)=> setUsername(e.target.value)}/>
-                <span className="text-red-700 text-xs">{usernameError}</span>
+                <label htmlFor="username" >Username</label>
+                <input type="text" value={values.username} name="username" onChange={(e) => handleOnChange(e)} onBlur={!isInitialForm ? validateForm : () => {}}/>
+                { errors.username && <span className="text-red-700 text-xs">{errors.username}</span> }
             </div>
             <div className="flex flex-col mt-1">
-                <label htmlFor="txtPasswords">Password</label>
-                <input type="password" value={password} name="txtPasswords" onChange={(e)=> setPassword(e.target.value)}/>
-                <span className="text-red-700 text-xs">{passwordError}</span>
+                <label htmlFor="password">Password</label>
+                <input type="password" value={values.password} name="password" onChange={(e) => handleOnChange(e)} onBlur={!isInitialForm ? validateForm : () => {}}/>
+                { errors.password && <span className="text-red-700 text-xs">{errors.password}</span> }
                 
             </div>
             <div className="mt-1">
                 <button type="submit" 
-                
-                className='bg-slate-500 px-1 hover:bg-amber-100 '>Login</button>
+                disabled={isSubmitting}
+                className={`bg-slate-500 px-1 hover:bg-amber-100 `}>Login</button>
                 <Link to="/" className='text-blue-500 text-sm ml-1'> back to home</Link>
             </div>
             </form>
-        </div>
+        </div> 
     </>
   )
 }
